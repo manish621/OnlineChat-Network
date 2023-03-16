a2c4aa0baf940ccd7c74475bf2f0d78fcffe5521
Manish234 <rajpurohitmanish067@gmail.com>
2023-03-16 10:26:42 +0530

    first commit

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+
+
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
